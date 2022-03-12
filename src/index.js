07369ede581b5329789f515e108219248ebecb6c
柴必青 <biqing.chai@weimob.com>
2022-03-13 01:16:23 +0800

    update /showmumu

--- src/index.js
+++ src/index.js
@@ -1,24 +1,22 @@
 // scroll bar
-import 'simplebar/src/simplebar.css';
-
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
-
+import { BrowserRouter } from 'react-router-dom';
+import 'simplebar/src/simplebar.css';
 //
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 import reportWebVitals from './reportWebVitals';
+import * as serviceWorker from './serviceWorker';
 
 // ----------------------------------------------------------------------
 
 ReactDOM.render(
   <HelmetProvider>
-    <BrowserRouter>
+    <BrowserRouter basename="/showmumu">
       <App />
     </BrowserRouter>
   </HelmetProvider>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
 
 // If you want to enable client cache, register instead.
